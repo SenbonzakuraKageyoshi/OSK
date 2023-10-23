24e8108f9a50e729d6de3d8ef8099b036e897144
Mark <markwebdev@mail.ru>
2023-10-23 22:33:58 +0300

    Last v

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const modalButtons = document.querySelectorAll('.section-catalog-list__item__link');
+    const closeModalButtons = document.querySelectorAll('.modal__content-close');
 
+    const hideModal = (target) => {
+        const parent = target.closest('.modal');
+        parent.classList.remove('active');
+    }
+    
+    const openModal = (target) => {
+        document.querySelector(`#${target.getAttribute('data-modal')}`).classList.add('active')
+    };
+
+    modalButtons.forEach((el) => el.addEventListener('click', (e) => openModal(e.target)));
+    closeModalButtons.forEach((el) => el.addEventListener('click', (e) => hideModal(e.target)));
 });
---
+++ modal-third.php
@@ -0,0 +1,44 @@
+<?php
+	$name = $_POST['username-first'];
+	$email = $_POST['email-first'];
+	$message = $_POST['message-first'];
+	$phone = $_POST['telephone-first'];
+
+	$to = "info@paint-chamber.ru"; 
+	$date = date ("d.m.Y"); 
+	$time = date ("h:i");
+	$subject = "Заявка c сайта";
+
+	
+	$msg="
+    Имя: $name /n
+	Email: $email /n
+    Телефон: $phone /n
+	Сообщение: $message";	
+	mail($to, $subject, $msg);
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./assets/images/favicons/32x32.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="images/favicons/64x64.svg" type="image/svg+xml">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <title itemprop="headline">Заявка принята</title>
+</head>
+<style>
+	body{
+		background: var(--gray);
+		height: 100vh;
+		display: flex;
+		align-items: center; 
+		justify-content: center;
+		flex-direction: column;
+	}
+</style>
+<div style="text-align: center; border-radius: 10px; height: 118px; background-color: var(--yellow); width: 340px; font-weight: 400; display: flex; align-items: center;justify-content: center; flex-direction: column;">
+	<p style="color: var(--gray)">Ваша заявка принята, скоро с Вами свяжутся</p>
+	<a href="/" style="color: var(--white); margin-top: 20px; display: block;">Вернуться на сайт</a>
+</div>
